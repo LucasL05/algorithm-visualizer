3f6fe093cc552d514bf4e776952b9752667957c3
Lucas <lalilucas05@gmail.com>
2026-08-08 12:21:39 -0300

    fix: calculate margins dynamically based on rectangle count and window width

--- main.c
+++ main.c
@@ -1,38 +1,46 @@
 #include <raylib.h>
 #include <stdio.h>
-#include <math.h>
+
 
 #define WIDTH 900
 #define HEIGHT 600
 #define GAP 2
 // #define MARGIN 16
 
-#define COUNT 200
+#define COUNT 99
 // #if (COUNT * (1 + GAP)) > USABLE_WIDTH
 //    #error "FATAL ERROR: Too many bars for this window width."
 // #endif
 
-int numbers[COUNT];
+int find_margin()
+{
+    for (int divisor = COUNT; divisor >= 1; divisor --)
+    {
+        if (WIDTH % divisor == 0 && (COUNT - divisor) % 2 == 0)
+        {
+            return COUNT - divisor;
+        }
+    }
+}
 
+int numbers[COUNT];
 void draw_bars() 
 {   
-    /* To get a beautiful, flawless grid, every single slot must be exactly the same width (e.g., exactly 4 pixels), 
-    which means we have to reverse our architecture.
-    Instead of hardcoding a MARGIN of 16 and forcing the bars to stretch into the messy leftover space, 
-    what if we calculate the perfect bar size first (WIDTH / COUNT), 
-    and let the margin absorb the leftover pixels? 
-    How would you write the math to automatically calculate a dynamic margin that centers the perfect grid on the screen?*/
-
-    /* def margin as width % (count - margin) = 0
-    maybe it'd also be nice to make margin always be even, but probably not.*/
+/* margin fixed! Now the only thing left to get even rectangles is probably related to the gap. Maybe I should incorporate
+it in the usable_width calculation, but I'm not sure. */
     for (int i = 0; i < COUNT; i++) 
     {
         int value = numbers[i] + 1;
         int bar_height = HEIGHT * 0.75 * value / (COUNT +1); // + 1 cause the value of numbers[i] = i + 1, since otherwise the first bar whould have height zero.
 
+        // Calculate margin and usable width
+        int margin = find_margin();
+        int usable_width = WIDTH - margin;
+        // *****put usable_width test here****
+
         // Calculate exact pixel boundaries for current and next bar
-        int x_current = ((i * USABLE_WIDTH) / COUNT) + MARGIN;
-        int x_next    = (((i + 1) * USABLE_WIDTH) / COUNT) + MARGIN;
+        int x_current = ((i * usable_width) / COUNT) + margin/2;
+        int x_next    = (((i + 1) * usable_width) / COUNT) + margin/2;
 
         // Bar width fills the space between current and next boundary minus the gap
         int bar_width = (x_next - x_current) - GAP;
