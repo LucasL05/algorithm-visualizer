6266d94a8a86e5a125d18ac56b116ff965e57c59
Lucas <lalilucas05@gmail.com>
2026-08-10 21:21:43 -0300

    refactor: replace margin calculation with a more robust algorithm

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <stdio.h>
+#include <assert.h>
 
 
 #define WIDTH 900
@@ -14,13 +15,14 @@
 
 int find_margin()
 {
-    for (int divisor = COUNT; divisor >= 1; divisor --)
-    {
-        if (WIDTH % divisor == 0 && (COUNT - divisor) % 2 == 0)
-        {
-            return COUNT - divisor;
-        }
-    }
+    int rest = WIDTH % COUNT;
+    if (rest % 2 == 0) return rest;
+    if (COUNT % 2 == 1) return rest + COUNT;
+    else return 0;
+    /* Maybe returninig 0 here isn't really the best option, but if rest isn't even
+    and COUNT is, there's really no way to make all bar's width equal by changing just the margin.
+    Maybe there's a way if I change the bar's width algorithm, but that may be complicated. */
+    
 }
 
 int numbers[COUNT];
@@ -28,19 +30,20 @@ void draw_bars()
 {   
 /* margin fixed! Now the only thing left to get even rectangles is probably related to the gap. Maybe I should incorporate
 it in the usable_width calculation, but I'm not sure. */
+    // Calculate margin and usable width
+    int margin = find_margin();
+    int usable_width = WIDTH - margin;
+    assert(COUNT * (1 + GAP) < usable_width && "ERROR: Too many bars for this window width."); // beware that assert is a no-op when NDEBUG is defined.
+        
+
     for (int i = 0; i < COUNT; i++) 
     {
         int value = numbers[i] + 1;
         int bar_height = HEIGHT * 0.75 * value / (COUNT +1); // + 1 cause the value of numbers[i] = i + 1, since otherwise the first bar whould have height zero.
 
-        // Calculate margin and usable width
-        int margin = find_margin();
-        int usable_width = WIDTH - margin;
-        // *****put usable_width test here****
-
         // Calculate exact pixel boundaries for current and next bar
-        int x_current = ((i * usable_width) / COUNT) + margin/2;
-        int x_next    = (((i + 1) * usable_width) / COUNT) + margin/2;
+        int x_current = (i * usable_width / COUNT) + margin/2;
+        int x_next    = ((i + 1) * usable_width / COUNT) + margin/2;
 
         // Bar width fills the space between current and next boundary minus the gap
         int bar_width = (x_next - x_current) - GAP;
