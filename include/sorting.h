4822dd19d6818cb850710a869dfbfbdc68ca5f13
Lucas <lalilucas05@gmail.com>
2026-08-19 20:50:19 -0300

    WIP: implement merge sort

--- include/sorting.h
+++ include/sorting.h
@@ -1,7 +1,7 @@
 #ifndef SORTING_H
 #define SORTING_H
 
-void merge_sort(void);
+void merge_sort(int *numbers, int l, int r);
 void quick_sort(void);
 
 #endif // SORTING_H
--- src/sorting.c
+++ src/sorting.c
@@ -2,22 +2,54 @@
 #include "bars.h"
 
 
-// I mean, this is weird, since I'm not actually mergin anything... should probably check this out later.
+// I mean, this is weird, since I'm not actually mergin anything... should probably check this out later. 
+//                                          !!! Well, well, well lol
 
 /* Sorts *numbers[*l*:*j*].
     l < j and j = l + 1 
 */
 void merge(int *numbers, int l, int r, int middle) {
-    for (int i = l; i <= middle; i ++) {
-        int j = middle + i;
-        if (numbers[i] > numbers[j]) {
-            int temp = numbers[j];
-            numbers[j] = numbers[l];
-            numbers[l] = temp;
-            printf("ola");
+    int temp[r - l + 1]; // this 1 would be necessary if the input was *the array's length**. Then it would be like Python.
+
+    int i = l;
+    int j = middle + 1; // this is where the second array starts
+    int t_index = 0;  // temporary array's index.
+    while (i <= middle && j <= r) { // This while loop definetly should be a subfunction. I wouldn't even neeed to define j and so on here. I'd be just passing them as parameters and changing their names on the subfuction's header.
+        if (numbers[i] < numbers[j]) {
+            temp[t_index] = numbers[i];
+            i += 1;
         }
+
+        else {
+            temp[t_index] = numbers[j];
+            j += 1;
+        }
+
+        t_index ++;
+    }
+
+    //verifies if some items in some list wasn't seen and inserts them in temp;
+    if (i <= middle) { // this one should also be a subfunction.
+        for (int x = i; i <= middle; x++ ) { // ****Does it make sense to define x as i?
+            temp[t_index] = numbers[x];
+            t_index++;
+        } 
+    }
+
+        if (j <= r) { // this one should also be a subfunction.
+        for (int x = i; i <= r; x++ ) { // ****Does it make sense to define x as i?
+            temp[t_index] = numbers[x];
+            t_index++;
+        } 
+    }
+
+
+    // r - l +1 is the temp array's size.
+    for (int i = 0; i < (r - l + 1); i ++) {
+        numbers[l + i] = temp[i];
     }
 }
+
 // *l* is the left index adn *r* is the right index
 // that contains the sub array to be sorted.
 // - If you'd like to sort the whole array, 
@@ -25,6 +57,7 @@ void merge(int *numbers, int l, int r, int middle) {
 // - *l* and *r* must be non-negative integers.
 void merge_sort(int *numbers, int l, int r)
 // check if *r* and *l* are positive. 
+// doesn't really work, but it's getting better
 {
     if (l < r) {
         int middle = (l + r) / 2;
